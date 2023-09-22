16bfd2e784fc1ae12dd0d74f8e7a2efb402cf75e
Patrick Varin <patrick.varin@figure.ai>
2023-09-22 01:22:01 +0000

    remove some comments

--- casadi/config.h.cmake
+++ casadi/config.h.cmake
@@ -29,18 +29,18 @@
 #define CASADI_PATCH_VERSION ${CASADI_PATCH_VERSION}
 #define CASADI_IS_RELEASE ${CASADI_IS_RELEASE}
 #define CASADI_VERSION_STRING "${CASADI_VERSION}"
-#define CASADI_GIT_REVISION "${git_revision}"  // NOLINT(whitespace/line_length)
-#define CASADI_GIT_DESCRIBE "${git_describe}"  // NOLINT(whitespace/line_length)
-#define CASADI_FEATURE_LIST "${feature_list}"  // NOLINT(whitespace/line_length)
-#define CASADI_BUILD_TYPE "${CMAKE_BUILD_TYPE}"  // NOLINT(whitespace/line_length)
-#define CASADI_COMPILER_ID "${CMAKE_CXX_COMPILER_ID}"  // NOLINT(whitespace/line_length)
-#define CASADI_COMPILER "${CMAKE_CXX_COMPILER}"  // NOLINT(whitespace/line_length)
-#define CASADI_COMPILER_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${UPPER_CMAKE_BUILD_TYPE}} ${EXTRA_CXX_FLAGS_FROM_DEFS}"  // NOLINT(whitespace/line_length)
-#define CASADI_MODULES "${CASADI_MODULES}"  // NOLINT(whitespace/line_length)
-#define CASADI_PLUGINS "${CASADI_PLUGINS}"  // NOLINT(whitespace/line_length)
-#define CASADI_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}"  // NOLINT(whitespace/line_length)
-#define CASADI_SHARED_LIBRARY_PREFIX "${CMAKE_SHARED_LIBRARY_PREFIX}"  // NOLINT(whitespace/line_length)
-#define CASADI_SHARED_LIBRARY_SUFFIX "${CMAKE_SHARED_LIBRARY_SUFFIX}"  // NOLINT(whitespace/line_length)
-#define CASADI_OBJECT_FILE_SUFFIX "${CMAKE_C_OUTPUT_EXTENSION}"  // NOLINT(whitespace/line_length)
+#define CASADI_GIT_REVISION "${git_revision}" 
+#define CASADI_GIT_DESCRIBE "${git_describe}" 
+#define CASADI_FEATURE_LIST "${feature_list}"
+#define CASADI_BUILD_TYPE "${CMAKE_BUILD_TYPE}"
+#define CASADI_COMPILER_ID "${CMAKE_CXX_COMPILER_ID}"
+#define CASADI_COMPILER "${CMAKE_CXX_COMPILER}" 
+#define CASADI_COMPILER_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${UPPER_CMAKE_BUILD_TYPE}} ${EXTRA_CXX_FLAGS_FROM_DEFS}" 
+#define CASADI_MODULES "${CASADI_MODULES}" 
+#define CASADI_PLUGINS "${CASADI_PLUGINS}"
+#define CASADI_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}"  
+#define CASADI_SHARED_LIBRARY_PREFIX "${CMAKE_SHARED_LIBRARY_PREFIX}"  
+#define CASADI_SHARED_LIBRARY_SUFFIX "${CMAKE_SHARED_LIBRARY_SUFFIX}"  
+#define CASADI_OBJECT_FILE_SUFFIX "${CMAKE_C_OUTPUT_EXTENSION}"  
 
 #endif  // CASADI_CONFIG_H // NOLINT(build/header_guard)
